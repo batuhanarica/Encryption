31095d0157ac67f909cf5f8bb880e8c1ddaf5443
batuhanarica <batuhanarica212@gmail.conm>
2025-07-02 16:29:36 +0300

    RSA commit

--- src/main/java/org/example/RSA.java
+++ src/main/java/org/example/RSA.java
@@ -9,6 +9,12 @@ public class RSA{
     private static PublicKey publicKey;
     private static PrivateKey privateKey;
 
+    /**
+     * Generates a new RSA key pair with a key size of 2048 bits and assigns the generated
+     * private and public keys to the corresponding class fields.
+     *
+     * @throws NoSuchAlgorithmException if the RSA algorithm is not available in the environment.
+     */
     public static void generateKeyRSA() throws NoSuchAlgorithmException {
         KeyPairGenerator generator = KeyPairGenerator.getInstance("RSA");
         generator.initialize(2048);
@@ -17,6 +23,14 @@ public class RSA{
         publicKey = pair.getPublic();
     }
 
+    /**
+     * Encrypts the given plaintext string using the RSA algorithm and the public key.
+     * If the public key is not initialized, it generates a new RSA key pair.
+     *
+     * @param plaintext the plain text string to be encrypted
+     * @return the encrypted text, encoded in Base64 format
+     * @throws Exception if an error occurs during encryption or key generation
+     */
     public static String encryptRSA(String plaintext) throws Exception {
         if (publicKey == null) {
             RSA.generateKeyRSA();  // Ensure keys are generated
